fe6af33b991c808a097c7713874aea6627075fe3
Maximilian Barabas <barabasmaximilian@gmail.com>
2023-04-25 14:39:04 +0200

    home almost done

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+/*
+* File: home.component.ts
+* Author: Balogh Csenge
+* Copyright: 2021, Balogh Csenge
+* Group: Szoft_II_N
+* Date: 2023-04-07
+* Github: https://github.com/csengekulker/
+* Licenc: GNU GPL
+*/
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
