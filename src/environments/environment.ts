93c20e07bc19b8e6cacf6a594740294bb6151d1d
Maximilian Barabas <barabasmaximilian@gmail.com>
2023-04-25 15:28:56 +0200

    final one

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+/*
+* File: environments.ts
+* Author: Maximilian Barabas
+* Copyright: M B
+* Group: Szoft_II_N
+* Date: 2023-04-23
+* Github: https://github.com/maximilianbarabas
+* Licenc: GNU GPL
+*/
+
+export const environment = {
+  production: false,
+  apihost: 'http://localhost:8000/'
+};
+
